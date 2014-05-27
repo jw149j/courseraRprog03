77043d55517a9b9f298a28cf80fd6b9d97cdf0da
jw149j <Jonathan.Wilkes@glasgow.ac.uk>
2014-05-27 16:32:53 +0100

    After considerable tinkering first draft of function best.R

--- best.R
+++ best.R
@@ -2,6 +2,8 @@ best <- function(state, outcome) {
 ## Iter001 - get error checking
 ## read in the outcome-of-care-measures dataframe  
 filtered<-read.csv("outcome-of-care-measures.csv",colClasses="character")
+## establish the outcomes to be valid and their associated column numbers:
+validOuts<- list("heart attack"=11,"heart failure"=17,"pneumonia"=23) 
 ## split the data on the state 2 letter identifier
 hh<-split(filtered,filtered$State)
 ## list the valid names in the data set error flagged if not
@@ -10,10 +12,23 @@ namit<-names(hh)
 if(!(state %in% namit)){stop("invalid state")}
 ## check the input outcome label against a vector of valid labels
 ## error flagged if not in the valid set
-if(!(outcome %in% c("heart attack","heart failure","pneumonia"))){stop("invalid outcome")}
+if(!(outcome %in% names(validOuts))){stop("invalid outcome")}
 
 ## Read outcome data
 ## Check that state and outcome are valid
 ## Return hospital name in that state with lowest 30-day death
+## Get the required state results as a dataframe
+thisData<-transform(hh[state])
+## Explicitly state the outcome required
+outy<-validOuts[outcome]
+## The required data has to be explicitly coerced to numeric!
+## This also produces NA conversions, throwing a warning which must ber supressed
+## use order to 1) order the numerical values of the variable 2) alphabetically by hospital name 
+suppressWarnings(thatData<-thisData[order(as.numeric(thisData[,as.numeric(outy[1])]),thisData[,2]),])
+off<-thatData[1,2]
+print(off)
+
+
+
 ## rate
 }
